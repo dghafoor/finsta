fad021eb6e030215e5666c30a1c2539f6c4e5717
dghafoor <42578071+dghafoor@users.noreply.github.com>
2022-03-07 16:53:03 -0600

    files to main

---
+++ ProfileViewController.swift
@@ -0,0 +1,34 @@
+//
+//  ProfileViewController.swift
+//  instagram
+//
+//  Created by user210615 on 3/7/22.
+//
+
+
+import UIKit
+ 
+class ProfileViewController: UIViewController {
+ 
+    override func viewDidLoad() {
+        super.viewDidLoad()
+ 
+        // Do any additional setup after loading the view.
+    }
+    
+    
+    @IBAction func onEditProfilePictureButton(_ sender: Any) {
+    }
+    
+ 
+    /*
+    // MARK: - Navigation
+ 
+    // In a storyboard-based application, you will often want to do a little preparation before navigation
+    override func prepare(for segue: UIStoryboardSegue, sender: Any?) {
+        // Get the new view controller using segue.destination.
+        // Pass the selected object to the new view controller.
+    }
+    */
+ 
+}
